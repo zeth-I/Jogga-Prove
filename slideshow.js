2180a376cc0f0d47efd0be88e05ee06c716fcdea
José Lucas | DevZeth <jlcbraga@outlook.com>
2021-02-06 13:38:58 -0300

    Correção

--- slideshow.js
+++ slideshow.js
@@ -11,7 +11,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
+  var slides = document.getElementsByClassName("slideshow");
   var dots = document.getElementsByClassName("ball");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
